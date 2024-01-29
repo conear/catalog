84c2805c94d2c2c81ade904c054ce5f1125805a7
Marcelo Adrian Slon <cone_ar@hotmail.com>
2024-01-29 19:05:29 -0300

    Codigo foto para revisar

--- src/components/MostraProduto.jsx
+++ src/components/MostraProduto.jsx
@@ -14,6 +14,7 @@ const {imgCode, description, marca, price} = produto;
         <p>Descrição: <strong>{description.toLowerCase()}</strong></p>
         <p>Marca: <strong>{marca.toLowerCase()}</strong></p>
         <p>Preço: <strong>{showPrice(price)}</strong></p>
+        <p><small>{imgCode}</small></p>
         <img src={`images/img_${imgCode}_hr.webp`} alt="" width={400}  title="Click para voltar" onClick={() => setProduto()} />
         
       </div>
