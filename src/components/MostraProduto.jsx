4d2ac68d196a0ac5584da4d284e8951356da09a6
Marcelo Slon <cone_ar@hotmail.com>
2024-05-06 18:25:14 -0300

    modificado padding da image e texto a direita

--- src/components/MostraProduto.jsx
+++ src/components/MostraProduto.jsx
@@ -8,14 +8,17 @@ const {imgCode, description, marca, price, qt} = produto;
 
   return (
    
-      <div>
-        <p>Descrição: <strong>{description.toLowerCase()}</strong></p>
-        <p>Marca: <strong>{marca.toLowerCase()}</strong></p>
-        <p>Preço: <strong>{showPrice(price)}</strong></p>
-        <p>Estoque: <strong>{qt}</strong></p>
-        <p><small>{imgCode}</small></p>
+      <div className="contenedor">
+        
+        {/* <p><small>{imgCode}</small></p> */}
         <img src={`images/${imgCode}`} alt="" width={400}  title="Click para voltar" onClick={() => setProduto()} />
-
+        <div className="texto">
+          <p>Descrição: <strong>{description.toLowerCase()}</strong></p>
+          <p>Marca: <strong>{marca.toLowerCase()}</strong></p>
+          <p>Preço: <strong>{showPrice(price)}</strong></p>
+          <p>Estoque: <strong>{qt}</strong></p>
+        </div>
+        
       </div>
     
   )
