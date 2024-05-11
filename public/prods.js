1e65ae0fba00716a7edd1573ff038b6d389b5432
Marcelo Slon <cone_ar@hotmail.com>
2024-05-11 10:19:52 -0300

    adicionados produtos e imagens

--- public/prods.js
+++ public/prods.js
@@ -1,10 +1,314 @@
 [
     {
-    "imgCode": "ARANDELA_ARA_MENO.webp",
+    "imgCode": "PENDENTE_PUNCTO_4203_SECTO.webp",
+    "type": "pendente",
+    "price": "consultar",
+    "marca": "secto",
+    "description": "PENDENTE PUNCTO 4203",
+    "qt": "1"
+    },
+    {
+    "imgCode": "EMBUTIDO_QUADRADO_BRANCO_PAR16_240101123.webp",
+    "type": "embutido",
+    "price": "consultar",
+    "marca": "",
+    "description": "EMBUTIDO QUADRADO BRANCO PAR16 240101123",
+    "qt": "2"
+    },
+    {
+    "imgCode": "EMBUTIDO_QUADRADO_BRANCO_AR70_240101124.webp",
+    "type": "embutido",
+    "price": "consultar",
+    "marca": "",
+    "description": "EMBUTIDO QUADRADO BRANCO AR70 240101124",
+    "qt": "1"
+    },
+    {
+    "imgCode": "ARANDELA_BIONDA_3771_BERTOLUCCI.webp",
     "type": "arandela",
+    "price": "consultar",
+    "marca": "bertolucci",
+    "description": "ARANDELA BIONDA 3771",
+    "qt": "1"
+    },
+    {
+    "imgCode": "LUMINARIA_MESA_URUCUM_MINI_AMARELA_BERTOLUCCI.webp",
+    "type": "mesa",
+    "price": "1000,00",
+    "marca": "bertolucci",
+    "description": "LUMINARIA MESA URUCUM MINI AMARELA",
+    "qt": "1"
+    },
+    {
+    "imgCode": "LUMINARIA_MESA_URUCUM_MINI_AZUL_BERTOLUCCI.webp",
+    "type": "mesa",
+    "price": "1000,00",
+    "marca": "bertolucci",
+    "description": "LUMINARIA MESA URUCUM MINI AZUL",
+    "qt": "3"
+    },
+    {
+    "imgCode": "LUMINARIA_MESA_URUCUM_MINI_VERMELHA_BERTOLUCCI.webp",
+    "type": "mesa",
+    "price": "1000,00",
+    "marca": "bertolucci",
+    "description": "LUMINARIA MESA URUCUM MINI VERMELHA",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_BIOMBO_CINZA_BERTOLUCCI.webp",
+    "type": "pendente",
+    "price": "3500,00",
+    "marca": "bertolucci",
+    "description": "PENDENTE BIOMBO CINZA",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_GONGUE_PRETO_BERTOLUCCI.webp",
+    "type": "pendente",
+    "price": "350,00",
+    "marca": "bertolucci",
+    "description": "PENDENTE GONGUE PRETO",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_MUBE_COBRE_BERTOLUCCI.webp",
+    "type": "pendente",
+    "price": "4500,00",
+    "marca": "bertolucci",
+    "description": "PENDENTE MUBE COBRE",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_NAU_PEQUENO_BERTOLUCCI.webp",
+    "type": "pendente",
+    "price": "1500,00",
+    "marca": "bertolucci",
+    "description": "PENDENTE NAU PEQUENO",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_PEQUIM_COBRE_BERTOLUCCI.webp",
+    "type": "pendente",
+    "price": "1500,00",
+    "marca": "bertolucci",
+    "description": "PENDENTE PEQUIM COBRE",
+    "qt": "2"
+    },
+    {
+    "imgCode": "PENDENTE_POLCA_BRANCO_BERTOLUCCI.webp",
+    "type": "pendente",
     "price": "500,00",
-    "marca": "meno",
-    "description": "ARANDELA ARA",
+    "marca": "bertolucci",
+    "description": "PENDENTE POLCA BRANCO",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_POLCA_VERMELHA_BERTOLUCCI.webp",
+    "type": "pendente",
+    "price": "500,00",
+    "marca": "bertolucci",
+    "description": "PENDENTE POLCA VERMELHA",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_UNIVERSO_LATÃO_DOURADO_BERTOLUCCI.webp",
+    "type": "pendente",
+    "price": "3500,00",
+    "marca": "bertolucci",
+    "description": "PENDENTE UNIVERSO LATÃO DOURADO",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_URUCUM_BRONZE_BERTOLUCCI.webp",
+    "type": "pendente",
+    "price": "700,00",
+    "marca": "bertolucci",
+    "description": "PENDENTE URUCUM BRONZE",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_URUCUM_COBRE_BERTOLUCCI.webp",
+    "type": "pendente",
+    "price": "1500,00",
+    "marca": "bertolucci",
+    "description": "PENDENTE URUCUM COBRE",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PLAFON_MUBE_COBRE_BERTOLUCCI.webp",
+    "type": "pendente",
+    "price": "3500,00",
+    "marca": "bertolucci",
+    "description": "PLAFON MUBE COBRE",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PLAFON_VIDRO_1_X100W_ELLA.webp",
+    "type": "plafon",
+    "price": "consultar",
+    "marca": "ella",
+    "description": "PLAFON VIDRO 1 X100W",
+    "qt": "2"
+    },
+    {
+    "imgCode": "PENDENTE_LOTTO_FABIAN.webp",
+    "type": "pendente",
+    "price": "1500,00",
+    "marca": "fabian",
+    "description": "PENDENTE LOTTO",
+    "qt": "1"
+    },
+    {
+    "imgCode": "BALIZADOR_TRACE_CINZA_ILUMINAR.webp",
+    "type": "balizador",
+    "price": "650,00",
+    "marca": "iluminar",
+    "description": "BALIZADOR TRACE CINZA",
+    "qt": "2"
+    },
+    {
+    "imgCode": "BALIZADOR_TRACE_PRETO_ILUMINAR.webp",
+    "type": "balizador",
+    "price": "650,00",
+    "marca": "iluminar",
+    "description": "BALIZADOR TRACE PRETO",
+    "qt": "2"
+    },
+    {
+    "imgCode": "FINCO_LED_35MM_PRETO_ILUMINAR.webp",
+    "type": 0,
+    "price": "consultar",
+    "marca": "iluminar",
+    "description": "FINCO LED 35MM PRETO",
+    "qt": "2"
+    },
+    {
+    "imgCode": "FINCO_SOLO_3_FOCO_PRETO_ILUMINAR.webp",
+    "type": 0,
+    "price": "consultar",
+    "marca": "iluminar",
+    "description": "FINCO SOLO 3 FOCO PRETO",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PLAFON_GEO_10X10_ILUMINAR.webp",
+    "type": "plafon",
+    "price": "531,00",
+    "marca": "iluminar",
+    "description": "PLAFON GEO 10X10",
+    "qt": "2"
+    },
+    {
+    "imgCode": "PLAFON_GEO_10X10X05_ILUMINAR.webp",
+    "type": "plafon",
+    "price": "537,00",
+    "marca": "iluminar",
+    "description": "PLAFON GEO 10X10X05",
+    "qt": "7"
+    },
+    {
+    "imgCode": "PLAFON_GEO_2797_01_ILUMINAR.webp",
+    "type": "plafon",
+    "price": "821,00",
+    "marca": "iluminar",
+    "description": "PLAFON GEO 2797.01",
+    "qt": "2"
+    },
+    {
+    "imgCode": "PLAFON_MAMBO_PRETO_ILUMINAR.webp",
+    "type": "plafon",
+    "price": "350,00",
+    "marca": "iluminar",
+    "description": "PLAFON MAMBO PRETO",
+    "qt": "1"
+    },
+    {
+    "imgCode": "SOLO_EMBUTIDO_ART._LED_PRETO_ILUMINAR.webp",
+    "type": 0,
+    "price": "consultar",
+    "marca": "iluminar",
+    "description": "SOLO EMBUTIDO ART. LED PRETO",
+    "qt": "5"
+    },
+    {
+    "imgCode": "SPOT_MAX_BRANCO_PAR_30_ILUMINAR.webp",
+    "type": "projetor",
+    "price": "#N/D",
+    "marca": "iluminar",
+    "description": "SPOT MAX BRANCO PAR 30",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_ATOOL_INGO_MAURER.webp",
+    "type": "pendente",
+    "price": "5500,00",
+    "marca": "ingo maurer",
+    "description": "PENDENTE ATOOL",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_FLOOP_CRISTALLO_KUNDALINI.webp",
+    "type": "pendente",
+    "price": "4500,00",
+    "marca": "kundalini",
+    "description": "PENDENTE FLOOP CRISTALLO",
+    "qt": "1"
+    },
+    {
+    "imgCode": "ARANDELA_POP_CROMADA_MARSET.webp",
+    "type": "arandela",
+    "price": "1500,00",
+    "marca": "marset",
+    "description": "ARANDELA POP CROMADA",
+    "qt": "1"
+    },
+    {
+    "imgCode": "BALIZADOR_AMANITA_MARSET.webp",
+    "type": "balizador",
+    "price": "500,00",
+    "marca": "marset",
+    "description": "BALIZADOR AMANITA",
+    "qt": "1"
+    },
+    {
+    "imgCode": "LUMINARIA_CHÃO_NOLITA_PRETA_MARSET.webp",
+    "type": "piso",
+    "price": "7500,00",
+    "marca": "marset",
+    "description": "LUMINARIA CHÃO NOLITA PRETA",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_MERCER_MARSET.webp",
+    "type": "pendente",
+    "price": "6000,00",
+    "marca": "marset",
+    "description": "PENDENTE MERCER",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_NOLITA_PRETO_MARSET.webp",
+    "type": "pendente",
+    "price": "5600,00",
+    "marca": "marset",
+    "description": "PENDENTE NOLITA PRETO",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_NOLITA_TRANSPARENTE_MARSET.webp",
+    "type": "pendente",
+    "price": "#N/D",
+    "marca": "marset",
+    "description": "PENDENTE NOLITA TRANSPARENTE",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_OLIVIA_BIANCO_MARSET.webp",
+    "type": "pendente",
+    "price": "3500,00",
+    "marca": "marset",
+    "description": "PENDENTE OLIVIA BIANCO",
     "qt": "1"
     },
     {
@@ -24,27 +328,27 @@
     "qt": "4"
     },
     {
-    "imgCode": "ARANDELA_POP_CROMADA_MARSET.webp",
-    "type": "arandela",
+    "imgCode": "PLAFON_MINI_JESOLO_E14_MELTEMI.webp",
+    "type": "plafon",
     "price": "1500,00",
-    "marca": "marset",
-    "description": "ARANDELA POP CROMADA",
+    "marca": "meltemi",
+    "description": "PLAFON MINI JESOLO E14",
+    "qt": "7"
+    },
+    {
+    "imgCode": "ARANDELA_ARA_NEMO.webp",
+    "type": "arandela",
+    "price": "500,00",
+    "marca": "nemo",
+    "description": "ARANDELA ARA",
     "qt": "1"
     },
     {
-    "imgCode": "ARANDELA_SWING_VIBIA.webp",
-    "type": "arandela",
-    "price": "1500,00",
-    "marca": "vibia",
-    "description": "ARANDELA SWING",
-    "qt": "1"
-    },
-    {
-    "imgCode": "LUMINARIA_LIGHT_VOLUME_F33_PRANDINA.webp",
-    "type": "piso",
-    "price": "4500,00",
-    "marca": "prandina",
-    "description": "LUMINARIA LIGHT VOLUME F33",
+    "imgCode": "PLAFON_EXT_REGULUS_TIGE_FLOUR_NEMO.webp",
+    "type": "plafon",
+    "price": "???",
+    "marca": "nemo",
+    "description": "PLAFON EXT REGULUS TIGE FLOUR",
     "qt": "1"
     },
     {
@@ -56,11 +360,35 @@
     "qt": "1"
     },
     {
-    "imgCode": "PENDENTE_ATOOL_INGO_MAURER.webp",
-    "type": "pendente",
-    "price": "5500,00",
-    "marca": "ingo maurer",
-    "description": "PENDENTE ATOOL",
+    "imgCode": "LUMINARIA_PISO_2X50W_364105_MODULAR.webp",
+    "type": "piso",
+    "price": "#N/D",
+    "marca": "modular",
+    "description": "LUMINARIA PISO 2X50W  364105",
+    "qt": "1"
+    },
+    {
+    "imgCode": "ARANDELA_LIGHT_VOLUME_PRANDINA.webp",
+    "type": "arandela",
+    "price": "2500,00",
+    "marca": "prandina",
+    "description": "ARANDELA LIGHT VOLUME",
+    "qt": "1"
+    },
+    {
+    "imgCode": "LUMINARIA_LIGHT_VOLUME_F33_PRANDINA.webp",
+    "type": "piso",
+    "price": "4500,00",
+    "marca": "prandina",
+    "description": "LUMINARIA LIGHT VOLUME F33",
+    "qt": "1"
+    },
+    {
+    "imgCode": "LUMINARIA_LIGHT_VOLUME_T22_PRANDINA.webp",
+    "type": 0,
+    "price": "6000,00",
+    "marca": "prandina",
+    "description": "LUMINARIA LIGHT VOLUME T22",
     "qt": "1"
     },
     {
@@ -69,14 +397,6 @@
     "price": "6500,00",
     "marca": "prandina",
     "description": "PENDENTE BILUNA S7 NERO",
-    "qt": "1"
-    },
-    {
-    "imgCode": "PENDENTE_FLOOP_CRISTALLO_KUNDALINI.webp",
-    "type": "pendente",
-    "price": "4500,00",
-    "marca": "kundalini",
-    "description": "PENDENTE FLOOP CRISTALLO",
     "qt": "1"
     },
     {
@@ -96,14 +416,6 @@
     "qt": "1"
     },
     {
-    "imgCode": "PENDENTE_NOLITA_PRETO_MARSET.webp",
-    "type": "pendente",
-    "price": "5600,00",
-    "marca": "marset",
-    "description": "PENDENTE NOLITA PRETO",
-    "qt": "1"
-    },
-    {
     "imgCode": "PENDENTE_NOTTE_PE_S9_BIANCO_PRANDINA.webp",
     "type": "pendente",
     "price": "8500,00",
@@ -120,12 +432,75 @@
     "qt": "1"
     },
     {
-    "imgCode": "PENDENTE_NOTTE_S3_BLACK/GOLD_LEAF_S/_CANOPLA_PRANDINA.webp",
+    "imgCode": "PENDENTE_NOTTE_S3_BLACK_GOLD_LEAF_S__CANOPLA_PRANDINA.webp",
     "type": "pendente",
     "price": "4500,00",
     "marca": "prandina",
     "description": "PENDENTE NOTTE S3 BLACK/GOLD LEAF S/ CANOPLA",
     "qt": "1"
+    },
+    {
+    "imgCode": "ARANDELA_SWING_VIBIA.webp",
+    "type": "arandela",
+    "price": "1500,00",
+    "marca": "vibia",
+    "description": "ARANDELA SWING",
+    "qt": "1"
+    },
+    {
+    "imgCode": "LUMINARIA_BRISA_VIBIA.webp",
+    "type": 0,
+    "price": "3500,00",
+    "marca": "vibia",
+    "description": "LUMINARIA BRISA",
+    "qt": "1"
+    },
+    {
+    "imgCode": "ARANDELA_NARCISO_COLOR.webp",
+    "type": "arandela",
+    "price": "consultar",
+    "marca": "",
+    "description": "ARANDELA NARCISO COLOR",
+    "qt": "1"
+    },
+    {
+    "imgCode": "ARANDELA_POLIPO_AZ_VD.webp",
+    "type": "arandela",
+    "price": "#N/D",
+    "marca": "",
+    "description": "ARANDELA POLIPO AZ VD",
+    "qt": "2"
+    },
+    {
+    "imgCode": "COLUNA_PISO_POLICARBONADO_OPACO.webp",
+    "type": "piso",
+    "price": "2000,00",
+    "marca": "",
+    "description": "COLUNA PISO POLICARBONADO OPACO",
+    "qt": "2"
+    },
+    {
+    "imgCode": "PENDENTE_CIMENTO_MEDIO_4661.webp",
+    "type": "pendente",
+    "price": "consultar",
+    "marca": "",
+    "description": "PENDENTE CIMENTO MEDIO 4661",
+    "qt": "1"
+    },
+    {
+    "imgCode": "PENDENTE_CIMENTO_PEQUENO_4660.webp",
+    "type": "pendente",
+    "price": "consultar",
+    "marca": "",
+    "description": "PENDENTE CIMENTO PEQUENO 4660",
+    "qt": "2"
+    },
+    {
+    "imgCode": "PENDENTE_PRETO_MADEIRA_SECTO.webp",
+    "type": "pendente",
+    "price": "#N/D",
+    "marca": "secto",
+    "description": "PENDENTE PRETO MADEIRA",
+    "qt": "1"
     }
     ]
-    
